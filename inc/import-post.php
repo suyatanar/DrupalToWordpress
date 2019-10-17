598bbb44b4db158287f675a27af187f52c24972d
suyatanar <suytnm@sph.com.sg>
2019-10-17 16:39:48 +0800

    Add category, tag and featured image import in import-post.php

--- inc/import-post.php
+++ inc/import-post.php
@@ -7,6 +7,32 @@ function import_post(){
 
 		if ($drupal_post_type == true && $wp_post_type == true) {
 			include ( plugin_dir_path( __FILE__ ).'db.php');
+			// $sql = "SELECT DISTINCT n.nid AS id,
+			// n.uid AS post_author,
+			// FROM_UNIXTIME(n.created) AS post_date,
+			// FROM_UNIXTIME(sc.publish_on) AS publish_on,
+			// CONVERT (bb.body_value USING utf8) AS post_content,
+			// n.title AS post_title,
+			// bb.body_summary AS post_excerpt,
+			// n.type AS post_type,
+			// IF(n.status = 1, 'publish', 'draft') AS post_status,
+			// a.alias AS alias,
+			// IF(f.entity_id IS NULL or f.entity_id = '', 0, f.entity_id) AS post_parent,
+			// td.name AS category,
+			// (REPLACE(fileURL.uri,'public://','https://media.herworld.com/public/')) AS featuredImage
+			// FROM node n
+			// LEFT JOIN url_alias a ON (REPLACE(a.source,'node/','') = n.nid)
+			// LEFT JOIN field_data_body bb ON bb.entity_id = n.nid
+			// LEFT JOIN field_data_field_gallery_media f ON f.field_gallery_media_nid = n.nid
+			// LEFT JOIN scheduler sc ON sc.nid = n.nid
+			// LEFT JOIN taxonomy_index t ON t.nid = n.nid
+			// LEFT JOIN taxonomy_term_data td ON td.tid = t.tid
+			// LEFT JOIN field_data_field_media_main_image mainImg ON mainImg.entity_id = n.nid
+			// LEFT JOIN field_data_field_img_file imgFile ON imgFile.entity_id = mainImg.field_media_main_image_nid
+			// LEFT JOIN file_managed fileURL ON fileURL.fid = imgFile.field_img_file_fid
+			// WHERE IF(n.status = 1, 'publish', 'draft') = 'publish'
+			// AND a.source LIKE 'node%' AND n.type = 'story' AND n.nid = 324076";
+
 			$sql = "SELECT DISTINCT n.nid AS id,
 			n.uid AS post_author,
 			FROM_UNIXTIME(n.created) AS post_date,
@@ -16,18 +42,30 @@ function import_post(){
 			bb.body_summary AS post_excerpt,
 			n.type AS post_type,
 			IF(n.status = 1, 'publish', 'draft') AS post_status,
+			
+			IF(td.vid = 2, td.name, '') AS category,
+			GROUP_CONCAT(IF(td.vid != 4, NUll, td.name)) AS tag,
 			a.alias AS alias,
 			IF(f.entity_id IS NULL or f.entity_id = '', 0, f.entity_id) AS post_parent,
-			td.name AS category
+			(REPLACE(fileURL.uri,'public://','https://media.herworld.com/public/')) AS featuredImage
 			FROM node n
 			LEFT JOIN url_alias a ON (REPLACE(a.source,'node/','') = n.nid)
 			LEFT JOIN field_data_body bb ON bb.entity_id = n.nid
 			LEFT JOIN field_data_field_gallery_media f ON f.field_gallery_media_nid = n.nid
 			LEFT JOIN scheduler sc ON sc.nid = n.nid
-			LEFT JOIN taxonomy_index t ON t.nid = n.nid
-			LEFT JOIN taxonomy_term_data td ON td.tid = t.tid
+
+			LEFT JOIN field_data_field_sections sec ON sec.entity_id = n.nid
+			LEFT JOIN field_data_field_tags tag ON tag.entity_id = n.nid
+
+			LEFT JOIN taxonomy_term_data td ON 
+			td.tid = sec.field_sections_tid
+			OR td.tid = tag.field_tags_tid 
+
+			LEFT JOIN field_data_field_media_main_image mainImg ON mainImg.entity_id = n.nid
+			LEFT JOIN field_data_field_img_file imgFile ON imgFile.entity_id = mainImg.field_media_main_image_nid
+			LEFT JOIN file_managed fileURL ON fileURL.fid = imgFile.field_img_file_fid
 			WHERE IF(n.status = 1, 'publish', 'draft') = 'publish'
-			AND a.source LIKE 'node%' AND n.type = 'story' AND td.vid = 2
+			AND a.source LIKE 'node%' AND n.type = 'story' AND n.nid = 319501		
 			LIMIT 100";
 
 			$result = mysqli_query($conn, $sql);
@@ -47,11 +85,14 @@ function import_post(){
 					$split_url = explode('/', $post_name);
 					$count = sizeof($split_url);
 					$post_name = $split_url[$count-1];
-					$category = $row['category'];
+					$post_cat = $row['category'];
+					$post_tag = $row['tag'];
 					$post_cat = get_cat_ID ('Ideas & Advice');
 					$post_parent = $row['post_parent'];						
 					$post_content = html_entity_decode($row['post_content']);
 					$post_content = str_replace("'", "\'", $post_content);
+					$featured_image_url = $row['featuredImage'];
+					$filetype = wp_check_filetype( basename( $featured_image_url ), null );
 
 					// import post
 					$my_post = array(
@@ -61,20 +102,44 @@ function import_post(){
 						'post_type'		=> $post_type,
 						'post_status'   => $post_status,
 						'post_author'   => $post_author,
-						'post_category' => array($post_cat),
+						'post_category' => $post_cat,
+						'tags_input'	=> $post_tag,
 						'post_date'		=> $post_date,
 						'post_excerpt'	=> $post_excerpt,
 						'post_name'		=> $post_name,
-						'post_parent'	=> $post_parent,
+						'post_parent'	=> $post_parent						
 					);
 
-					
-					$impot_post = wp_insert_post( $my_post, true );
+
+					$impot_post = wp_insert_post( $my_post, true );					
 
 				}
 
 				if ($impot_post == true) {
-					//var_dump($impot_post);
+					// Insert the attachment.
+					// Get the path to the upload directory.
+					$wp_upload_dir = wp_upload_dir();
+
+					// Prepare an array of post data for the attachment.
+					$attachment = array(
+						'guid'           => $wp_upload_dir['url'] . '/' . basename( $featured_image_url ), 
+						'post_mime_type' => $filetype['type'],
+						'post_title'     => preg_replace( '/\.[^.]+$/', '', basename( $featured_image_url ) ),
+						'post_content'   => '',
+						'post_status'    => 'inherit'
+					);
+
+					$attach_id = wp_insert_attachment( $attachment, $featured_image_url, $post_id );
+
+					// Make sure that this file is included, as wp_generate_attachment_metadata() depends on it.
+					require_once( ABSPATH . 'wp-admin/includes/image.php' );
+
+					// Generate the metadata for the attachment, and update the database record.
+					$attach_data = wp_generate_attachment_metadata( $attach_id, $featured_image_url );
+					wp_update_attachment_metadata( $attach_id, $attach_data );
+
+					set_post_thumbnail( $post_id, $attach_id );
+
 					wp_redirect('options-general.php?page=import_data&status=Complete');
 				}
 				else{
