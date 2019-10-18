76ee06b0f43b267d6543620126aa35cc18898fdd
suyatanar <suytnm@sph.com.sg>
2019-10-18 11:35:43 +0800

    Fixed bug db setting file

--- inc/db.php
+++ inc/db.php
@@ -4,19 +4,21 @@ global $wpdb;
 $table = $wpdb->prefix . "import_data";
 $db_config = $wpdb->get_results( "SELECT * FROM $table ORDER BY `t_id` DESC LIMIT 1" );
 
-foreach ($db_config as $db_config) {
-	$host = $db_config->host_name;
-	$db_name = $db_config->db_name;
-	$db_username = $db_config->db_username;
-	$db_password = $db_config->db_password;
+if ($db_config == true) {
+	foreach ($db_config as $db_config) {
+		$host = $db_config->host_name;
+		$db_name = $db_config->db_name;
+		$db_username = $db_config->db_username;
+		$db_password = $db_config->db_password;
+	}
+
+	// Create connection
+	$conn = mysqli_connect($host, $db_username, $db_password, $db_name);
+
+	// Check connection
+	if (!$conn) {
+		die("Connection failed: " . mysqli_connect_error());
+	}
+
+	mysqli_set_charset($conn,"utf8");
 }
-
-// Create connection
-$conn = mysqli_connect($host, $db_username, $db_password, $db_name);
-
-// Check connection
-if (!$conn) {
-	die("Connection failed: " . mysqli_connect_error());
-}
-
-mysqli_set_charset($conn,"utf8");
